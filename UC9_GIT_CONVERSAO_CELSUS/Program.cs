de90d3a58cb9d2d8ed0ea707fa294d0b016f94d0
andre.lgpinto <andregomeees0@gmail.com>
2023-03-07 20:17:07 -0300

    conversao graus

--- UC9_GIT_CONVERSAO_CELSUS/Program.cs
+++ UC9_GIT_CONVERSAO_CELSUS/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double temperatura = 0;
+string opcao = "C";
+double conversao = 0;
+
+Console.WriteLine("Qual o valor da temperatura que deseja converter?");
+temperatura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Deseja converter para qual unidade de temperatura?");
+Console.WriteLine("C - Graus CELSIUS");
+Console.WriteLine("F - Graus FAHRENHEIT");
+opcao = Console.ReadLine();
+
+Console.WriteLine("");
+
+switch (opcao)
+{
+    case "C":
+        // (0 ºC x 9/5) + 32 = 32 ºF
+        conversao = (temperatura * 9 / 5) + 32;
+        break;
+
+    case "F":
+        // (0 ºF -32) x 5/9 = -17,78 ºC
+        conversao = (temperatura - 32) * 9 / 5;
+        break;
+
+        default:
+
+        break;
+}
+Console.WriteLine("Temperatura convertida é: " + conversao);
